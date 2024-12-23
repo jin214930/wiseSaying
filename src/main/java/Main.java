5f941c3db524854db3456b59b3e3185b4289af90
jin214930 <jin214930@naver.com>
2024-12-23 15:30:36 +0900

    work2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         App app = new App();
@@ -7,7 +9,14 @@ public class Main {
 
 class App {
     public void run() {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("== 명언 앱 ==");
+        System.out.println("명령) 등록");
+        System.out.print("명언 : ");
+        scanner.nextLine();
+        System.out.print("작가 : ");
+        scanner.nextLine();
         System.out.println("명령) 종료");
     }
 }
