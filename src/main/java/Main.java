3452211060fc65e6824b4ab0c43e621e617d4146
jin214930 <jin214930@naver.com>
2024-12-23 15:44:13 +0900

    work3 - complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,18 +12,19 @@ class App {
         Scanner scanner = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
 
-        while(true) {
+        while (true) {
             System.out.print("명령) ");
             String command = scanner.nextLine();
 
-            if (command.equals("등록")) {
+            if (command.equals("종료")) {
+                System.out.println("명언 앱을 종료합니다.");
+                break;
+            } else if (command.equals("등록")) {
                 System.out.print("명언 : ");
                 scanner.nextLine();
                 System.out.print("작가 : ");
                 scanner.nextLine();
                 System.out.println("1번 명언이 등록되었습니다.");
-            } else if (command.equals("종료")) {
-                return;
             }
         }
     }
