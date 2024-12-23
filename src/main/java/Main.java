4e264040da7f5beb35557e08da146dbf5b522919
jin214930 <jin214930@naver.com>
2024-12-23 15:31:12 +0900

    work3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,6 +17,7 @@ class App {
         scanner.nextLine();
         System.out.print("작가 : ");
         scanner.nextLine();
+        System.out.println("1번 명언이 등록되었습니다.");
         System.out.println("명령) 종료");
     }
 }
