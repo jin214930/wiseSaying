3e0441e1a542165bea9c8a3a7b9c3ce4370860ba
jin214930 <jin214930@naver.com>
2024-12-24 11:47:07 +0900

    work5 - method & constructor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,12 +8,17 @@ public class Main {
 }
 
 class App {
+    int lastId = 0;
+    int wiseSayingSize = 0;
+    WiseSaying[] wiseSayingList = new WiseSaying[5];
+
     public void run() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
 
-        int lastIdx = 0;
-        WiseSaying[] wiseSayingList = new WiseSaying[3];
+        // 테스트 데이터
+        add("현재를 사랑하라.", "작자미상");
+        add("과거에 집착하지 마라.", "작자미상");
 
         while (true) {
             System.out.print("명령) ");
@@ -27,23 +32,21 @@ class App {
                 String content = scanner.nextLine();
                 System.out.print("작가 : ");
                 String author = scanner.nextLine();
-
-                WiseSaying wiseSaying = new WiseSaying();
-                wiseSaying.id = ++lastIdx;
-                wiseSaying.content = content;
-                wiseSaying.author = author;
-                wiseSayingList[lastIdx - 1] = wiseSaying;
-
-                System.out.printf("%d번 명언이 등록되었습니다.\n", lastIdx);
+                add(content, author);
+                System.out.printf("%d번 명언이 등록되었습니다.\n", lastId);
             } else if (command.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
-                for (int i = lastIdx - 1; i >= 0; i--) {
-                    WiseSaying wiseSaying = wiseSayingList[i];
-                    System.out.printf("%d / %s / %s\n", wiseSaying.id, wiseSaying.author, wiseSaying.content);
+                for (int i = wiseSayingSize - 1; i >= 0; i--) {
+                    System.out.printf("%d / %s / %s\n", wiseSayingList[i].id, wiseSayingList[i].author, wiseSayingList[i].content);
                 }
             }
         }
+    }
+
+    public void add(String content, String author) {
+        WiseSaying wiseSaying = new WiseSaying(++lastId, content, author);
+        wiseSayingList[wiseSayingSize++] = wiseSaying;
     }
 }
 
@@ -51,4 +54,10 @@ class WiseSaying {
     int id;
     String content;
     String author;
+
+    WiseSaying(int id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
 }
