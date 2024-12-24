311b21a5a382401d519fc0f1b09f931bb0e857c6
jin214930 <jin214930@naver.com>
2024-12-24 15:41:19 +0900

    work6

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     private int lastId = 0;
-    private int wiseSayingSize = 0;
-    private final WiseSaying[] wiseSayingList = new WiseSaying[5];
+    private final ArrayList<WiseSaying> wiseSayingList = new ArrayList<>();
     private final Scanner scanner = new Scanner(System.in);
 
 
@@ -25,13 +25,26 @@ public class App {
                 registerWiseSaying();
             } else if (command.equals("목록")) {
                 printWiseSayingList();
+            } else if (command.startsWith("삭제?id=")) {
+                int deleteId = Integer.parseInt(command.substring(6));
+                if (deleteWiseSaying(deleteId)) {
+                    System.out.printf("%d번 명언이 삭제되었습니다.", deleteId);
+                } else {
+                    System.out.printf("%d번 명언은 존재하지 않습니다.", deleteId);
+                }
             }
         }
     }
 
-    private void add(String content, String author) {
-        WiseSaying wiseSaying = new WiseSaying(++lastId, content, author);
-        wiseSayingList[wiseSayingSize++] = wiseSaying;
+    private boolean deleteWiseSaying(int deleteId) {
+        for (WiseSaying wiseSaying : wiseSayingList) {
+            if (wiseSaying.getId() == deleteId) {
+                wiseSayingList.remove(wiseSaying);
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private void registerWiseSaying() {
@@ -43,12 +56,17 @@ public class App {
         System.out.printf("%d번 명언이 등록되었습니다.\n", lastId);
     }
 
+
     private void printWiseSayingList() {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("----------------------");
-        for (int i = wiseSayingSize - 1; i >= 0; i--) {
-            WiseSaying wiseSaying = wiseSayingList[i];
-            System.out.printf("%d / %s / %s\n", wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getAuthor());
+        for (WiseSaying wiseSaying : wiseSayingList) {
+            System.out.printf("%d / %s / %s\n", wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent());
         }
+    }
+
+    private void add(String content, String author) {
+        WiseSaying wiseSaying = new WiseSaying(++lastId, content, author);
+        wiseSayingList.add(wiseSaying);
     }
 }
